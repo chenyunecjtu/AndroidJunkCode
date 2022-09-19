39e7765d1ca0156c917a3f111a4dc286c06727f5
chenyun <1159807248@qq.com>
2022-09-19 13:38:06 +0800

    no message

--- app/src/main/java/cn/hx/plugin/junkcode/demo/MainActivity.kt
+++ app/src/main/java/cn/hx/plugin/junkcode/demo/MainActivity.kt
@@ -2,7 +2,9 @@ package cn.hx.plugin.junkcode.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.Keep
 
+@Keep
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
